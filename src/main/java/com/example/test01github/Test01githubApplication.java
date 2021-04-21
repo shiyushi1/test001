f40792b315fa4edadc10e2fab688290b8b482438
sys <3420152006@qq.com>
2021-04-21 14:58:37 +0800

    test02

---
+++ src/main/java/com/example/test01github/Test01githubApplication.java
@@ -0,0 +1,13 @@
+package com.example.test01github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01githubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test01githubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test01github/Test01githubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test01github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test01githubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
